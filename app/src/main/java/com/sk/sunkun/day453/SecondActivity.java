0402a4286900ca156d7686432802cb19d37fdf7b
aa <aa>
2016-09-30 15:20:34 +0800

    SecondActivity页面的initView（）方法

--- app/src/main/java/com/sk/sunkun/day453/SecondActivity.java
+++ app/src/main/java/com/sk/sunkun/day453/SecondActivity.java
@@ -9,5 +9,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        initView();
+    }
+
+    private void initView() {
+        
     }
 }
