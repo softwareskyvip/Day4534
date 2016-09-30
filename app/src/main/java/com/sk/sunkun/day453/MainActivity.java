ff44364cab8eed70c56cc2261f809621920af219
aa <aa>
2016-09-30 14:22:33 +0800

    initData();

---
+++ app/src/main/java/com/sk/sunkun/day453/MainActivity.java
@@ -0,0 +1,21 @@
+package com.sk.sunkun.day453;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
+
+    private void initData() {
+    }
+
+    private void initView() {
+    }
+}
