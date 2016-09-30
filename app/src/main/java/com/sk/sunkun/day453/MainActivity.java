77432720ef20a333aaa76769012ffcb83bc5914a
aa <aa>
2016-09-30 14:23:28 +0800

    int a = 9;

--- app/src/main/java/com/sk/sunkun/day453/MainActivity.java
+++ app/src/main/java/com/sk/sunkun/day453/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initView();
         initData();
+        int a = 9;
     }
 
     private void initData() {
